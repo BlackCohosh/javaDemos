4615519b0430cf81319bb468f5715700c3bce3a3
Agriscapes <62765843+Agriscapes@users.noreply.github.com>
2021-12-21 11:18:53 -0600

    Initial commit

---
+++ springBoot-fundamentals/fundamentals/src/main/java/com/blackcohosh/fundamentals/FundamentalsApplication.java
@@ -0,0 +1,14 @@
+package com.blackcohosh.fundamentals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentalsApplication.class, args);
+		System.out.println("Suh, dude");
+	}
+
+}
